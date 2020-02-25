6e874cf7c9ad9712a72b3b785b87ebb703fc209c
Nick <nhunsaker20@gmail.com>
2020-02-25 15:20:40 -0700

    Finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import CurrenyConverter from './Components/CurrencyConverter'
+import CurrencyDisplay from './Components/CurrencyDisplay'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <h2>Render Currency</h2>
+        <CurrenyConverter render={(currencyData, amount) => (
+          <CurrencyDisplay amount={amount} currencyData={currencyData} />
+        )} />
       </div>
     );
   }
